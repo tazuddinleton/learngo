bdcbf5f0a0cca1a996fe690db870b06baa3ceb79
Taz Uddin <getleton@gmail.com>
2021-09-11 04:23:51 +0600

    Structs

--- main.go
+++ main.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+type Vertex struct {
+	X, Y int
+}
+
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+	v1 := Vertex{1, 2}
+	v2 := Vertex{3, 4}
+	fmt.Println(v1, v2)
+	fmt.Println(v1.X, v2.X)
+	fmt.Println(v2.Y, v1.Y)
+
+	// Struct pointer
+	v3 := &v1
+	v3.X, v3.Y = 10, 20
+	fmt.Println(v1)
+
+	// struct literals
+	structLiterals()
+}
+
+func structLiterals() {
+	var (
+		v1 = Vertex{1, 2}
+		v2 = Vertex{X: 1}
+		v3 = Vertex{}
+		p  = &Vertex{1, 2}
+	)
+
+	p.X = 1000
+	fmt.Println(v1, v2, v3, *p)
 }
