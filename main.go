58fba546d3bb2d25de8d314e62aa5d4f2dab9397
Taz Uddin <getleton@gmail.com>
2021-09-10 09:55:34 +0600

    variables

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+	var a = "hello"
+	var b = "world"
+	c := a + " " + b
+	fmt.Println(c)
+
+	var x, y, z int = 10, 10, 10
+	fmt.Println(x + y + z)
+
+	p := 10
+	q := 10
+	r := 20
+
+	fmt.Println(p + q + r)
+
+	isOk := false
+	isOk = !isOk
+	print(isOk)
+
+}
+
+func print(a ...interface{}) {
+	fmt.Println(a...)
 }
