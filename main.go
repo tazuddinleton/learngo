5eb23ea8a835e200cd36c50ad51e35c2fdd1035a
Taz Uddin <getleton@gmail.com>
2021-09-11 04:09:00 +0600

    Pointers

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+
+	i, j := 42, 2701
+
+	p := &i
+
+	*p = 21
+	pp := &p
+	ppp := &pp
+
+	fmt.Println(j)
+	fmt.Println(i)
+	fmt.Println(p)
+	fmt.Println(pp)
+	fmt.Println(ppp)
+	fmt.Println(***ppp)
+	fmt.Println(**pp)
+	fmt.Println(*p)
 }
