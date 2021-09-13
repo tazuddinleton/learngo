963f65ae238003d831f3ac29b2bacaa2b3f0d9de
Taz Uddin <getleton@gmail.com>
2021-09-13 18:03:51 +0600

    closure example

--- main.go
+++ main.go
@@ -2,10 +2,73 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+type Logger struct {
+	Info  func(string) int
+	Debug func(string) int
+	Error func(string) int
+}
+
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+
+	runLogExample()
+}
+
+func runLogExample() {
+	log := Logger{
+		Info:  GetLogger(0),
+		Debug: GetLogger(1),
+		Error: GetLogger(-1),
+	}
+
+	log.Info("hello world")
+	log.Debug("hello world")
+	log.Debug("hello world")
+	log.Info("hello world")
+	log.Error("hello world")
+	log.Info("hello world")
+	log.Info("hello world")
+	log.Info("hello default world")
+}
+
+// GetLogger returns log function based on level
+func GetLogger(level int) func(string) int {
+	logCount := 0
+	switch level {
+	case 0:
+		return func(s string) int {
+			logCount += 1
+			fmt.Printf("info: (%v) %v\n", logCount, s)
+			return logCount
+		}
+	case 1:
+		return func(s string) int {
+			logCount += 1
+			fmt.Printf("debug: (%v) %v\n", logCount, s)
+			return logCount
+		}
+	default:
+		return func(s string) int {
+			logCount += 1
+			fmt.Printf("error: (%v) %v\n", logCount, s)
+			return logCount
+		}
+	}
+}
+
+func multiPlyEach() {
+	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(ForEach(s, func(x int) int {
+		return x * 2
+	}))
+}
+
+// ForEach takes a slice and a func to apply to each element, returns
+func ForEach(s []int, fn func(int) int) []int {
+	res := make([]int, len(s))
+	for i := 0; i < len(s); i++ {
+		res[i] = fn(s[i])
+	}
+	return res
 }
