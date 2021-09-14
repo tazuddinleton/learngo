777e66839f670ebc46cb5f2de105d636b61af435
Taz Uddin <getleton@gmail.com>
2021-09-14 15:46:30 +0600

    methods

--- main.go
+++ main.go
@@ -2,10 +2,56 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+	p := Person{}
+
+	v := &p
+
+	v.SetFirstName("asdfasdf")
+
+	fmt.Println(v)
+}
+
+func (v *Vertex) Scale(s float64) {
+	v.x = v.x * s
+	v.y = v.y * s
+}
+
+func abs(v Vertex) float64 {
+	return math.Sqrt(v.x*v.x + v.y*v.y)
+}
+
+func (v Vertex) abs() float64 {
+	return math.Sqrt(v.x*v.x + v.y*v.y)
+}
+
+type Vertex struct {
+	x, y float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+// Value reciever
+func (p Person) SetFirstName(s string) {
+	p.FirstName = s
+}
+
+func SetFirstName(p Person, s string) {
+	p.FirstName = s
 }
