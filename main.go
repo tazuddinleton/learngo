a6c50e3890ad6b8a3e36419ceb074eb5861bc86f
Taz Uddin <getleton@gmail.com>
2021-09-15 09:42:13 +0600

    finish image excersize

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"image"
+	"image/color"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+
+}
+
+type Image struct {
+	w, h int
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.w, img.h)
+}
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+func (img Image) At(x, y int) color.Color {
+	return color.RGBA{0, 0, 255, 255}
 }
