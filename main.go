342882b4e35bcd20a73e18a6fc2349cf18b297c2
Taz Uddin <getleton@gmail.com>
2021-09-20 12:22:04 +0600

    test channel

--- main.go
+++ main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("The time now is:", time.Now())
+	var wg sync.WaitGroup
+	wg.Add(10)
+	ch := make(chan int)
+
+	for i := 0; i < 10; i++ {
+		go func(v int) {
+			ch <- v
+			fmt.Println("value written", v)
+			wg.Done()
+			fmt.Println("done called", v)
+		}(i)
+	}
+
+	go func() {
+		fmt.Println("waiting...")
+		wg.Wait()
+		fmt.Println("wait over closing channel...")
+		close(ch)
+	}()
+
+	Display(ch)
+}
+
+func Display(ch chan int) {
+	fmt.Println("Display got called")
+	for i := range ch {
+		fmt.Println("received", i)
+	}
+	fmt.Println("Display exiting...")
+
 }
