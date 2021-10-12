147b3dc0f534b362fc1cdd0d209a30c0ee1f22f6
Taz Uddin <getleton@gmail.com>
2021-10-13 00:01:26 +0600

    organize errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,3 +1,7 @@
+/**
+Errors package shows examples of errors in golang
+*/
+
 package errors
 
 import (
@@ -6,8 +10,9 @@ import (
 	"time"
 )
 
+// Run runs all the examples of current package
 func Run() {
-	i, err := doubleIt(10)
+	i, err := DoubleIt(10)
 
 	if err != nil {
 		fmt.Println(err)
@@ -15,24 +20,26 @@ func Run() {
 		fmt.Println(i)
 	}
 	x := 25.0
-	j, err := sqrt(x)
+	j, err := Sqrt(x)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println("sqrt of %v is %v", x, j)
+		fmt.Println(fmt.Printf("sqrt of %v is %v", x, j))
 	}
 
 	fmt.Println(ErrNegativeSqrt(-2).Error())
 }
 
-func doubleIt(x int64) (int64, error) {
+// DoubleIt doubles the positive integers and returns it, returns errors and zero value if negative
+func DoubleIt(x int64) (int64, error) {
 	if x < 0 {
 		return 0, NegativeNumberError{Message: "We don't support doubling negative numbers", TimeStamp: time.Now()}
 	}
 	return x * 2, nil
 }
 
-func sqrt(x float64) (float64, error) {
+// Sqrt returns square root of float64 positive numbers, error with zero value if negative
+func Sqrt(x float64) (float64, error) {
 	if x < 0 {
 		return 0, ErrNegativeSqrt(x)
 	}
@@ -40,16 +47,20 @@ func sqrt(x float64) (float64, error) {
 }
 
 type NegativeNumberError struct {
+	// Defines a negative number error object
 	TimeStamp time.Time
 	Message   string
 }
 
+// Error is the implementation of the interface error by the type NegativeNumberError
 func (err NegativeNumberError) Error() string {
 	return fmt.Sprintf("%v. Error happen at %s", err.Message, err.TimeStamp)
 }
 
+// ErrNegativeSqrt is a float64 type
 type ErrNegativeSqrt float64
 
+// Error is the implementation of the interface error by the type ErrNegativeSqrt
 func (v ErrNegativeSqrt) Error() string {
 	return fmt.Sprintf("We don't Sqrt negative numbers: %v", int64(v))
 }
