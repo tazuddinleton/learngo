2cc205bfa28198115ad977462e439d0ee1a74bed
Taz Uddin <tazuddinleton@gmail.com>
2022-04-26 02:05:52 +0600

    Add package pointers

---
+++ pointers/pointers.go
@@ -0,0 +1,23 @@
+package pointers
+
+import "fmt"
+
+func Run() {
+
+	i, j := 42, 2701
+
+	p := &i
+
+	*p = 21
+	pp := &p
+	ppp := &pp
+
+	fmt.Println(j)
+	fmt.Println(i)
+	fmt.Println(p)
+	fmt.Println(pp)
+	fmt.Println(ppp)
+	fmt.Println(***ppp)
+	fmt.Println(**pp)
+	fmt.Println(*p)
+}
